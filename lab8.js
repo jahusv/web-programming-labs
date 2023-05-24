48342047fbdd67241cc68a4ec69ffa9b8111da7f
Anastasia Panchuk <jahusv@gmail.com>
2023-05-24 12:01:47 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -28,3 +28,11 @@ function cleanDaysCount() {
     let output = document.getElementById('result');
     output.innerHTML = '';
 }
+
+function showTime (){
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+}
+
+setInterval(showTime, 1000);
